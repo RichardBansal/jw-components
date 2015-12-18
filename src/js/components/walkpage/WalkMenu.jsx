fb3ee20372c2b4ddbf7833618286257b83381efb
Richard Bansal <richard.bansal@gmail.com>
2015-12-18 12:33:43 -0500

    1. updating WalkMenu with correct text

--- src/js/components/walkpage/WalkMenu.jsx
+++ src/js/components/walkpage/WalkMenu.jsx
@@ -3,13 +3,19 @@ import { dateFormatted } from '../itinerary/ItineraryUtils';
 
 
 //TODO: Duplicate of Itinerary <Walk/> and WalkPage <WalkHeader/>, refactor/combine components into factory
-//TODO: Cleanup display of tags, perhaps using a simple lookup
 
 const WalkMenu = ({walk}) => {
 
   let {checkboxes, title, map, time, team} = walk;
   const walkLeader = team.find(member => member.role === 'walk-leader');
   const tags = Object.keys(checkboxes).filter(item => item.includes("theme-civic"));
+
+  const tagsRef = {
+    "theme-civic-goodneighbour":"Community",
+    "theme-civic-international":"International Issues",
+    "theme-civic-religion":"Religion",
+  };
+
   const menuItems = ['About This Walk', 'Walk Route', 'Accessibility', 'Taking Public Transit', 'How to find us', 'About the Walk Team'];
 
   return (
@@ -26,7 +32,7 @@ const WalkMenu = ({walk}) => {
         </ul>
       </section>
       <section id="tags">
-        {tags.map(tag => <span>#{tag}, </span>)}
+        {tags.map(tag => <span>#{tagsRef[tag]}, </span>)}
       </section>
     </section>
   );
