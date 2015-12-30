8961b3c7f4a23b6b83ddd140653d2d3ab87347de
Richard Bansal <richard.bansal@gmail.com>
2015-12-29 21:48:43 -0500

    1. Using filters from static data 2. adding WalkAccessibility/PublicTransit/Parking to menu 3. creating WalkParking component

--- src/js/components/walkpage/WalkPage.jsx
+++ src/js/components/walkpage/WalkPage.jsx
@@ -8,24 +8,25 @@ import WalkDescription from './WalkDescription.jsx';
 import WalkRoute from './WalkRoute.jsx';
 import WalkAccessibility from './WalkAccessibility.jsx';
 import WalkPublicTransit from './WalkPublicTransit.jsx';
+import WalkParking from './WalkParking.jsx';
 import WalkStart from './WalkStart.jsx';
 import WalkTeam from './WalkTeam.jsx';
 import WalkMenu from './WalkMenu.jsx';
 import WalkMap from './WalkMap.jsx';
 
-import {walk} from './WalkStaticData';
+import {walk, filters} from './WalkStaticData';
 import './view.less';
 
 const walkId = walk.walk.id;
 const itineraryListId = ItineraryStore.getItineraryList().id; //for stubbed data, assumed first list is Itinerary
 const favoriteListId = ItineraryStore.getFavouriteList().id; //for stubbed data, assumed second list is fav
 
-//TODO: Conditionals (? and ||) in getWalk are for stubbed data
-
 const getWalk = (props) => ({
+  //TODO: Conditionals (? and ||) in getWalk are for stubbed data
   walk: props.walk || walk.walk,
   page: props.page || walk.page,
   id: props.walk ? props.walk.id : walkId,
+  filters: props.filters || filters,
   existsInItinerary : ItineraryStore.existsInList(itineraryListId, props.walk ? props.walk.id : walkId),
   existsInFavourites : ItineraryStore.existsInList(favoriteListId, props.walk ? props.walk.id : walkId),
 });
@@ -69,14 +70,15 @@ export default class WalkPage extends React.Component {
 
       <WalkRoute {...this.state.walk}/>
 
-      <WalkAccessibility {...this.state.walk}/>
+      <WalkAccessibility {...this.state.walk} {...this.state.filters} style="walk-page"/>
 
-      <WalkPublicTransit {...this.state.walk}/>
+      <WalkPublicTransit {...this.state.walk} style="walk-page"/>
+
+      <WalkParking {...this.state.walk} style="walk-page"/>
 
       <WalkStart {...this.state.walk}/>
 
       <WalkTeam {...this.state.walk}/>
-      
     
     </div>);
   }
